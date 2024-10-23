26943850858a8fcb2c8584462c285c9ce451c381
Sergey <Zaikouski.sergey@gmail.com>
2024-10-23 19:00:04 +0300

    setup: fix Test

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Button } from "@leetvin/ui-kit";
-import s from "src/pages/test/Test.module.scss";
+import s from "./Test.module.scss";
 
 export const Test = () => {
   return (
     <div className={s.wrapper}>
       TEST
-      <Button variant={"primary"}>TEST</Button>
       <Button variant={"tertiary"}>TEST</Button>
       <Button variant={"secondary"}>TEST</Button>
       <Button variant={"link"}>TEST</Button>
