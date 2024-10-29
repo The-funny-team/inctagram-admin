4f51d704b3d07327d5bdddecac79c63d8da56d3d
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-10-29 23:55:53 +0100

    FUN-59 feat: add Header & useTranslation hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import { HeadMeta } from '@/shared/ui/HeadMeta'
-import { Test } from '@/widgets/test/Test'
 
 import '@funnyteam/ui-kit/style.css'
 export default function Home() {
   return (
     <>
       <HeadMeta title={'Home page'} />
-
-      <main>
-        Welcome! This is new app!
-        <Test />
-      </main>
+      <main>Welcome! This is new app!</main>
     </>
   )
 }
