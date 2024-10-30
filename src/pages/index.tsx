059b2d5b7f1d330c43d6f4029ea381f973769ed5
Sergey <Zaikouski.sergey@gmail.com>
2024-10-30 19:21:16 +0200

    FUN-60 fix: replace routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from 'react'
 
+import { ROUTES_URL } from '@/shared/const'
 import { getRootLayout } from '@/shared/layouts'
 import { useRouter } from 'next/router'
 
 import '@funnyteam/ui-kit/style.css'
 
 const Page = () => {
-  const auth = true // temporary variable
+  const auth = false // temporary variable
   const router = useRouter()
 
   useEffect(() => {
     if (!auth) {
-      void router.push('/sign-in')
+      void router.push(ROUTES_URL.SIGN_IN)
     } else {
-      void router.push('/users-list')
+      void router.push(ROUTES_URL.USERS_LIST)
     }
   }, [auth, router])
 
