07f15318a5150eaccd21523dac08f9b617abd855
Sergey <Zaikouski.sergey@gmail.com>
2024-11-08 17:36:16 +0200

    FUN-64 fix: delete unused svg

--- src/widgets/UsersList/UsersList.tsx
+++ src/widgets/UsersList/UsersList.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 
-import { BanIcon } from '@/shared/assets/icons/dropdown-icons/BanIcon'
-import { DeleteUserIcon } from '@/shared/assets/icons/dropdown-icons/DeleteUserIcon'
-import { DotsIcon } from '@/shared/assets/icons/dropdown-icons/DotsIcon'
 import { useDebounce } from '@/shared/lib/hooks'
-import { UsersListDropdown } from '@/widgets/UsersListDropdown'
 import { Input, Pagination, Select, TableEmpty } from '@funnyteam/ui-kit'
 
 import s from './UsersList.module.scss'
@@ -46,10 +42,6 @@ export const UsersList = () => {
 
   return (
     <>
-      <DotsIcon />
-      <DeleteUserIcon />
-      <BanIcon />
-      <UsersListDropdown userId={25} />
       <main>
         <div className={s.wrapper}>
           <div className={s.filters}>
