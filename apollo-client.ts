d2c78740b5905f1ae051ad3bdaf37343874a63e7
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-08 23:40:28 +0100

    FUN-68 feat: add .env file with login variables

--- apollo-client.ts
+++ apollo-client.ts
@@ -5,6 +5,7 @@ import { getMainDefinition } from '@apollo/client/utilities'
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import { createClient } from 'graphql-ws'
+import * as process from 'process'
 
 const httpLink = createHttpLink({
   uri: 'https://inctagram.work/api/v1/graphql',
@@ -17,8 +18,8 @@ const wsLink = new GraphQLWsLink(
 )
 
 const getCredentials = () => {
-  const username = ''
-  const password = ''
+  const username = process.env.NEXT_PUBLIC_USER_NAME
+  const password = process.env.NEXT_PUBLIC_PASSWORD
 
   return username && password ? btoa(`${username}:${password}`) : null
 }
