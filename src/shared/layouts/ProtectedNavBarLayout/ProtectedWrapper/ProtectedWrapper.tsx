595b58109bec16d5d87f4e2e68d7947330d3a282
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-15 04:15:25 +0100

    FUN-82 fix: ProtectedWrapper & LoadFromLS

--- src/shared/layouts/ProtectedNavBarLayout/ProtectedWrapper/ProtectedWrapper.tsx
+++ src/shared/layouts/ProtectedNavBarLayout/ProtectedWrapper/ProtectedWrapper.tsx
@@ -1,17 +1,20 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 
 import { ROUTES_URL } from '@/shared/const'
 import { loadFromLocalStorage } from '@/shared/lib/helpers'
-import { Loader } from '@/shared/ui/Loader'
 import { useRouter } from 'next/router'
 
 export const ProtectedWrapper = ({ children }: PropsWithChildren<{}>) => {
-  const isAuth = loadFromLocalStorage('isAuth', '')
+  const isAuth = loadFromLocalStorage('isAuth', false)
   const router = useRouter()
 
-  if (!isAuth) {
-    void router.push(ROUTES_URL.SIGN_IN)
+  useEffect(() => {
+    if (!isAuth) {
+      void router.push(ROUTES_URL.SIGN_IN)
+    }
+  }, [isAuth, router])
 
+  if (!isAuth) {
     return null
   }
 
--- src/shared/lib/helpers/LoadFromLocaleStorage.ts
+++ src/shared/lib/helpers/LoadFromLocaleStorage.ts
@@ -1,4 +1,7 @@
 export const loadFromLocalStorage = <T>(key: string, defaultValue: T): T => {
+  if (typeof window === 'undefined') {
+    return defaultValue
+  }
   const valueAsString = localStorage.getItem(key)
 
   return valueAsString !== null ? (JSON.parse(valueAsString) as T) : defaultValue
