c3d519128e4c9713db52a71b997a8043a89f1736
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-15 03:05:44 +0100

    FUN-79 feat: add pagination

--- src/widgets/User/UserMain/UserTabsContent/Followers/Followers.tsx
+++ src/widgets/User/UserMain/UserTabsContent/Followers/Followers.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 import { useGetFollowersQuery } from '@/queries/user/user.generated'
+import { PAGINATION_OPTIONS } from '@/shared/const'
 import { useTranslation } from '@/shared/lib/hooks'
+import { Loader } from '@/shared/ui/Loader'
 import { SortDirection } from '@/types'
 import { SortByType } from '@/widgets/UsersList/UsersList'
-import { Table, TableBody, Typography } from '@funnyteam/ui-kit'
+import { Pagination, Table, TableBody, Typography } from '@funnyteam/ui-kit'
 
 import s from './Followers.module.scss'
 
@@ -16,12 +18,14 @@ type Props = {
 export const Followers = ({ userId }: Props) => {
   const [sortBy, setSortBy] = useState<SortByType>('createdAt')
   const [sortDirection, setSortDirection] = useState<SortDirection>(SortDirection.Desc)
+  const [pageNumber, setPageNumber] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(8)
   const { text } = useTranslation()
   const t = text.pages.user.tabs.followers.emptyTab
   const { data, loading } = useGetFollowersQuery({
     variables: {
-      page: 1,
-      pageSize: 8,
+      page: pageNumber,
+      pageSize,
       sortBy,
       sortDirection,
       userId,
@@ -37,6 +41,15 @@ export const Followers = ({ userId }: Props) => {
   }) => {
     setSortBy(sortParams.newSortBy)
     setSortDirection(sortParams.newDirection)
+  }
+
+  const handlePageSize = (pageSize: string) => {
+    setPageSize(Number(pageSize))
+    setPageNumber(1)
+  }
+
+  if (loading) {
+    return <Loader />
   }
 
   return (
@@ -57,6 +70,16 @@ export const Followers = ({ userId }: Props) => {
               {followers?.map(f => <FollowersTableRow follower={f} key={f.id} />)}
             </TableBody>
           </Table>
+          <div className={s.pagination}>
+            <Pagination
+              currentPage={pageNumber}
+              onChangePage={setPageNumber}
+              onValueChange={handlePageSize}
+              options={PAGINATION_OPTIONS}
+              pageSize={pageSize}
+              totalCount={followersCount}
+            />
+          </div>
         </div>
       )}
     </div>
