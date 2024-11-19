2b6a5fd39d03627ff7599b2eacc4e097126c6abd
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-20 00:09:58 +0100

    FUN-86 feat: add head for PaymentsTable

--- src/widgets/PaymentsList/PaymentsList.tsx
+++ src/widgets/PaymentsList/PaymentsList.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 
+import { SortDirection } from '@/types'
 import { Checkbox, Input } from '@funnyteam/ui-kit'
 
 import s from './PaymentsList.module.scss'
+
+import { PaymentsListTable } from './PaymentsListTable'
 
 export const PaymentsList = () => {
   const [searchTerm, setSearch] = useState<string>('')
@@ -19,6 +22,11 @@ export const PaymentsList = () => {
       <div className={s.searchInput}>
         <Input onValueChange={handleSearch} type={'search'} value={searchTerm} />
       </div>
+      <PaymentsListTable
+        direction={SortDirection.Asc}
+        onDirectionChange={() => {}}
+        sortBy={'userName'}
+      />
     </div>
   )
 }
---
+++ src/widgets/PaymentsList/PaymentsListTable/PaymentsListTable.tsx
@@ -0,0 +1,85 @@
+import { SortActiveIcon, SortDefaultIcon } from '@/shared/assets'
+import { SortDirection } from '@/types'
+import { Table, TableHead, TableHeadCell, TableRow, Typography } from '@funnyteam/ui-kit'
+
+import s from './PaymentsListTable.module.scss'
+
+export type SortPaymentsType = 'amount' | 'createdAt' | 'paymentMethod' | 'userName'
+
+type PropsType = {
+  direction: SortDirection
+  onDirectionChange: (sortParams: {
+    newDirection: SortDirection
+    newSortBy: SortPaymentsType
+  }) => void
+  sortBy: SortPaymentsType
+}
+export const PaymentsListTable = ({ direction, onDirectionChange, sortBy }: PropsType) => {
+  const toggleSort = (newSortBy: SortPaymentsType) => {
+    const newDirection = direction === SortDirection.Asc ? SortDirection.Desc : SortDirection.Asc
+
+    onDirectionChange({ newDirection, newSortBy })
+  }
+
+  return (
+    <div className={s.tableWrapper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>
+              <div className={s.sortCell} onClick={() => toggleSort('userName')}>
+                <Typography as={'span'} variant={'boldText14'}>
+                  Username
+                </Typography>
+                {direction === SortDirection.Asc && sortBy === 'userName' ? (
+                  <SortActiveIcon />
+                ) : (
+                  <SortDefaultIcon />
+                )}
+              </div>
+            </TableHeadCell>
+            <TableHeadCell>
+              <div className={s.sortCell} onClick={() => toggleSort('createdAt')}>
+                <Typography as={'span'} variant={'boldText14'}>
+                  Date added
+                </Typography>
+                {direction === SortDirection.Asc && sortBy === 'userName' ? (
+                  <SortActiveIcon />
+                ) : (
+                  <SortDefaultIcon />
+                )}
+              </div>
+            </TableHeadCell>
+            <TableHeadCell>
+              <div className={s.sortCell} onClick={() => toggleSort('amount')}>
+                <Typography as={'span'} variant={'boldText14'}>
+                  Amount, $
+                </Typography>
+                {direction === SortDirection.Asc && sortBy === 'createdAt' ? (
+                  <SortActiveIcon />
+                ) : (
+                  <SortDefaultIcon />
+                )}
+              </div>
+            </TableHeadCell>
+            <TableHeadCell>
+              <Typography variant={'boldText14'}>Subscription</Typography>
+            </TableHeadCell>
+            <TableHeadCell>
+              <div className={s.sortCell} onClick={() => toggleSort('paymentMethod')}>
+                <Typography as={'span'} variant={'boldText14'}>
+                  Payment Method
+                </Typography>
+                {direction === SortDirection.Asc && sortBy === 'createdAt' ? (
+                  <SortActiveIcon />
+                ) : (
+                  <SortDefaultIcon />
+                )}
+              </div>
+            </TableHeadCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </div>
+  )
+}
