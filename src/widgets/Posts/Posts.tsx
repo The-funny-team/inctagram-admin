12ba14255d575d35da8d0d5b580c7737fe8678f4
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-23 21:17:21 +0100

    FUN-90 feat: add Input search

--- src/widgets/Posts/Posts.tsx
+++ src/widgets/Posts/Posts.tsx
@@ -1,3 +1,22 @@
+import { useState } from 'react'
+
+import { useDebounce } from '@/shared/lib/hooks'
+import { Input } from '@funnyteam/ui-kit'
+
+import s from './Posts.module.scss'
+
 export const Posts = () => {
-  return <div>Posts</div>
+  const [searchTerm, setSearchTerm] = useState<string>('')
+
+  const debouncedValue = useDebounce(searchTerm, 500)
+  const handleSearch = (value: string) => {
+    setSearchTerm(prevState => value)
+  }
+
+  return (
+    <div className={s.wrapper}>
+      <Input onValueChange={setSearchTerm} type={'search'} value={searchTerm} />
+      <div className={s.posts}>Posts</div>
+    </div>
+  )
 }
