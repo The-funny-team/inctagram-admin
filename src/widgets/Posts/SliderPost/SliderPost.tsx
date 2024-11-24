853432c88e8b62e242983ce0fb5d98acb2af7fb7
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-11-24 22:15:44 +0100

    FUN-88 feat: add Slider for Post

--- src/widgets/Posts/SliderPost/SliderPost.tsx
+++ src/widgets/Posts/SliderPost/SliderPost.tsx
@@ -1,3 +1,90 @@
-export const SliderPost = () => {
-  return <div>SliderPost</div>
+import React, { ComponentProps, ReactNode, useState } from 'react'
+import SlickSlider, { Settings } from 'react-slick'
+
+import { ArrowLeftShortIcon, ArrowRightShortIcon } from '@/shared/assets'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import s from './SliderPost.module.scss'
+
+import { SliderButton, SliderDot } from './SliderButtons'
+
+type Props = {
+  children: ReactNode
+  isDots: boolean
+  setSlideId?: (id: number) => void
+  sizeBtn: number
+  slideId?: number
+  sliderLength: number
+}
+export const SliderPost = ({
+  children,
+  isDots,
+  setSlideId,
+  sizeBtn,
+  slideId,
+  sliderLength,
+}: Props) => {
+  const [slideIdR, setSlideIdR] = useState<number>(0)
+
+  const beforeChangeHandler = (currentSlide: number, nextSlide: number) => {
+    if (setSlideId) {
+      setSlideId(nextSlide)
+    } else {
+      setSlideIdR(nextSlide)
+    }
+  }
+
+  const usedSlideId = slideId ? slideId : slideIdR
+
+  const settings: Settings = {
+    arrows: sliderLength > 1,
+    beforeChange: beforeChangeHandler,
+    customPaging: index => {
+      return <SliderDot isActive={usedSlideId === index} />
+    },
+    dots: isDots,
+    dotsClass: s.slickDots,
+    infinite: false,
+    nextArrow: (
+      <Btn
+        disabled={sliderLength - 1 === usedSlideId || sliderLength === 0}
+        isRight
+        size={sizeBtn}
+      />
+    ),
+    prevArrow: <Btn disabled={usedSlideId === 0} isRight={false} size={sizeBtn} />,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 500,
+    swipe: false,
+    swipeToSlide: false,
+  }
+
+  return (
+    <SlickSlider {...settings} className={s.slider}>
+      {children}
+    </SlickSlider>
+  )
+}
+
+type BtnType = {
+  disabled?: boolean
+  isRight: boolean
+  onClick?: ComponentProps<'button'>['onClick']
+  size: number
+}
+
+const Btn = ({ disabled, isRight, onClick, size }: BtnType) => {
+  return (
+    <SliderButton
+      className={isRight ? s.nextBtn : s.prevBtn}
+      disabled={disabled}
+      onClick={onClick}
+      size={size}
+    >
+      {isRight ? <ArrowRightShortIcon /> : <ArrowLeftShortIcon />}
+    </SliderButton>
+  )
 }
