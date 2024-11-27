90c4a127b510743089497e5a016cdc0bc4e2e36f
Sergey <Zaikouski.sergey@gmail.com>
2024-11-27 20:47:41 +0200

    FUN-90-1 feat: add scroll, check empty array, fix handleSearch

--- src/widgets/Posts/Posts.tsx
+++ src/widgets/Posts/Posts.tsx
@@ -1,30 +1,39 @@
 import { useEffect, useState } from 'react'
 
 import { useGetAllPostsQuery, usePostAddedSubscription } from '@/queries/posts/posts.generated'
-import { useDebounce } from '@/shared/lib/hooks'
+import { useDebounce, useTranslation } from '@/shared/lib/hooks'
 import { PostItem, PostType } from '@/widgets/Posts/Post/Post'
-import { Input } from '@funnyteam/ui-kit'
+import { Input, Typography } from '@funnyteam/ui-kit'
 
 import s from './Posts.module.scss'
 
 export const Posts = () => {
+  const { text } = useTranslation()
+  const t = text.pages.posts
   const [allPosts, setAllPosts] = useState<PostType[]>([])
   const [searchTerm, setSearchTerm] = useState<string>('')
   const [endCursorPostId, setEndCursorPostId] = useState(0)
 
   const debouncedValue = useDebounce(searchTerm, 500)
-  const { data: posts, refetch } = useGetAllPostsQuery({
+
+  const {
+    data: posts,
+    loading,
+    refetch,
+  } = useGetAllPostsQuery({
     variables: { endCursorPostId, searchTerm: debouncedValue },
   })
   const { data: newPostAdded } = usePostAddedSubscription()
 
   const handleSearch = (value: string) => {
+    setAllPosts([])
+    setEndCursorPostId(0)
     setSearchTerm(prevState => value)
   }
 
   useEffect(() => {
     if (posts && posts?.getPosts.items.length) {
-      setAllPosts(prevState => [...prevState, ...posts.getPosts.items])
+      setAllPosts([...allPosts, ...posts.getPosts.items])
     }
   }, [posts])
 
@@ -34,11 +43,35 @@ export const Posts = () => {
     }
   }, [newPostAdded])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop
+      const windowHeight = window.innerHeight
+      const fullHeight = document.documentElement.scrollHeight
+
+      if (windowHeight + scrollTop >= fullHeight - 100) {
+        const lastId = allPosts[allPosts?.length - 1].id
+
+        setEndCursorPostId(lastId)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [allPosts])
+
   return (
     <div className={s.wrapper}>
       <Input onValueChange={handleSearch} type={'search'} value={searchTerm} />
       <div className={s.posts}>
-        {allPosts?.map(p => <PostItem key={p.createdAt} post={p} refetch={refetch} />)}
+        {allPosts.length !== 0 ? (
+          allPosts.map(p => <PostItem key={p.id} post={p} refetch={refetch} />)
+        ) : (
+          <Typography variant={'regularText16'}>{t.noResults}</Typography>
+        )}
       </div>
     </div>
   )
