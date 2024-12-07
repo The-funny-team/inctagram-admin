e10bdf2e9d88722a31d2919a0f085464f0341118
Sergey <Zaikouski.sergey@gmail.com>
2024-12-07 18:01:01 +0200

    FUN-94 feat: add infinity scroll for show all photos

--- src/widgets/User/UserMain/UserTabsContent/Uploaded/Uploaded.tsx
+++ src/widgets/User/UserMain/UserTabsContent/Uploaded/Uploaded.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react'
+
 import { useGetUploadedPhotosQuery } from '@/queries/user/user.generated'
 import { useTranslation } from '@/shared/lib/hooks'
+import { ImagePost } from '@/types'
 import { Typography } from '@funnyteam/ui-kit'
 import Image from 'next/image'
 
@@ -11,22 +14,54 @@ type Props = {
 export const Uploaded = ({ userId }: Props) => {
   const { text } = useTranslation()
   const t = text.pages.user.tabs.uploadPhotos
-  const { data, loading } = useGetUploadedPhotosQuery({
+  const [allPhotos, setAllPhotos] = useState<ImagePost[]>([])
+  const [endCursorId, setEndCursorId] = useState(0)
+
+  const { data: photos, loading } = useGetUploadedPhotosQuery({
     skip: !userId,
     variables: {
+      endCursorId: endCursorId,
       userId,
     },
   })
 
-  const photos = data?.getPostsByUser.items
+  useEffect(() => {
+    if (photos && photos.getPostsByUser.items?.length) {
+      setAllPhotos([...allPhotos, ...photos.getPostsByUser.items])
+    }
+  }, [photos])
+
+  useEffect(() => {
+    if (loading) {
+      return
+    }
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop
+      const windowHeight = window.innerHeight
+      const fullHeight = document.documentElement.scrollHeight
+
+      if (windowHeight + scrollTop >= fullHeight - 100) {
+        const lastId = allPhotos[allPhotos?.length - 1].id || 0
+
+        setEndCursorId(lastId)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [allPhotos])
+
   const skeletonArray = Array.from({ length: 8 }, (_, index) => index)
 
   return (
     <div className={s.images}>
       {loading &&
         skeletonArray.map((_, index) => <Skeleton height={'235px'} key={index} width={'230px'} />)}
-      {photos?.length ? (
-        photos?.map(p => (
+      {allPhotos?.length ? (
+        allPhotos?.map(p => (
           <div className={s.imageItem} key={p.id}>
             <Image alt={'post'} fill src={p.url ?? ''} />
           </div>
